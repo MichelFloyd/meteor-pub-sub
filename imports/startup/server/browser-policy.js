6ce74d787e26885f500e48e395961d4af827c837
MichelFloyd <michel.floyd@cloaklabs.com>
2016-12-27 19:20:46 -0800

    Fixed issue with fonts not loading (also committed same fix to base)

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,2 +1,5 @@
-// import { BrowserPolicy } from 'meteor/browser-policy-common';
-// e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+import { BrowserPolicy } from 'meteor/browser-policy-common';
+/* insert browser policy rules here
+ e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+  */
+BrowserPolicy.content.allowFontOrigin("data:");
