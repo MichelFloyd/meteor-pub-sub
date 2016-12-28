72672eef1abbd6a14fa52734816e871cdf9ad9fb
MichelFloyd <michel.floyd@cloaklabs.com>
2016-12-27 19:40:07 -0800

    Create 1000 docs, one per second, on startup. Highlights reactivity.

--- imports/startup/server/createDocuments.js
+++ imports/startup/server/createDocuments.js
@@ -1,1 +1,11 @@
-import { upsertDocument } from '../api/documents/methods.js';
+// Create 1000 dummy documents, one per second, to highlight reactivity
+import {  Meteor } from 'meteor/meteor';
+import Documents from '../../api/documents/documents';
+
+Documents.remove({}); // start with a blank slate
+let i = 0;
+const interval = Meteor.setInterval(()=>{
+  Documents.insert({ title: "Document No. " + i, body: "Inserted at " + new Date() });
+  i++;
+  if ( i > 1000 ) Meteor.clearInterval(interval);
+},1000);
