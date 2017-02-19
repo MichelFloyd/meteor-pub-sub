f5b948cb2d7482af8abc3b4c2066890aa70cba8a
MichelFloyd <michel.floyd@cloaklabs.com>
2017-02-18 21:35:33 -0800

    Fixes issue #1

--- imports/ui/containers/ViewDocument.js
+++ imports/ui/containers/ViewDocument.js
@@ -1,14 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import { composeWithTracker } from 'react-komposer';
-import Documents from '../../api/documents/documents.js';
-import ViewDocument from '../pages/ViewDocument.js';
-import Loading from '../components/Loading.js';
+import Documents from '../../api/documents/documents';
+import ViewDocument from '../components/ViewDocument';
+import Loading from '../components/Loading';
 
 const composer = ({ params }, onData) => {
   const subscription = Meteor.subscribe('documents.view', params._id);
 
   if (subscription.ready()) {
-    const doc = Documents.findOne();
+    const doc = Documents.findOne(params._id);
     onData(null, { doc });
   }
 };
