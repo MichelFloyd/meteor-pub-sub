e27227b056a33b4c0b212aa628ddf2a96e7b00f7
MichelFloyd <michel.floyd@cloaklabs.com>
2017-02-20 15:56:19 -0800

    Simplified logic for dealing with missing document in the component

--- imports/ui/components/ViewDocument.js
+++ imports/ui/components/ViewDocument.js
@@ -19,32 +19,29 @@ const handleRemove = (_id) => {
   }
 };
 
-const ViewDocument = ({ doc }) => (
-  <div className="ViewDocument">
-    { doc ?
-      <div>
-        <Alert>
-          This shows a single document. The publication that this view subscribes to includes the <em>body</em> and <em>createdAt</em> keys which are not included in the list view publication.
-        </Alert>
-        <div className="page-header clearfix">
-          <h4 className="pull-left">{ doc.title }</h4>
-          <ButtonToolbar className="pull-right">
-            <ButtonGroup bsSize="small">
-              <Button href={`/documents/${doc._id}/edit`}>Edit</Button>
-              <Button onClick={ () => handleRemove(doc._id) } className="text-danger">Delete</Button>
-            </ButtonGroup>
-          </ButtonToolbar>
-        </div>
-        <em>_id:</em> {  doc._id }
-        <em>body:</em> { doc.body }<br/>
-        <em>createdAt:</em> { doc.createdAt && doc.createdAt.toLocaleString() }<br/>
-        <RelatedDocumentsList documentId={ doc._id }/>
+const ViewDocument = ({ doc }) => {
+  if (!doc) return <NotFound />;
+  return (
+    <div className="ViewDocument">
+      <Alert>
+        This shows a single document. The publication that this view subscribes to includes the <em>body</em> and <em>createdAt</em> keys which are not included in the list view publication.
+      </Alert>
+      <div className="page-header clearfix">
+        <h4 className="pull-left">{ doc.title }</h4>
+        <ButtonToolbar className="pull-right">
+          <ButtonGroup bsSize="small">
+            <Button href={`/documents/${doc._id}/edit`}>Edit</Button>
+            <Button onClick={ () => handleRemove(doc._id) } className="text-danger">Delete</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
       </div>
-    :
-      <NotFound/>
-    }
-  </div>
-);
+      <em>_id:</em> {  doc._id }
+      <em>body:</em> { doc.body }<br/>
+      <em>createdAt:</em> { doc.createdAt && doc.createdAt.toLocaleString() }<br/>
+      <RelatedDocumentsList documentId={ doc._id }/>
+    </div>
+  );
+};
 
 ViewDocument.propTypes = {
   doc: React.PropTypes.object,
