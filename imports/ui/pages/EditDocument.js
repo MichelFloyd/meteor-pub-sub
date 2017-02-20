46affd71bbf6153ed027166d67fb71e9ad9f6c10
MichelFloyd <michel.floyd@cloaklabs.com>
2017-02-20 15:46:38 -0800

    Added error handling in the EditDocument component to deal with the document not existing at all.

--- imports/ui/pages/EditDocument.js
+++ imports/ui/pages/EditDocument.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import DocumentEditor from '../components/DocumentEditor.js';
+import DocumentEditor from '../components/DocumentEditor';
+import NotFound from '../../ui/pages/NotFound';
 
 const EditDocument = ({ doc }) => (
-  <div className="EditDocument">
-    <h4 className="page-header">Editing "{ doc.title }"</h4>
-    <DocumentEditor doc={ doc } />
+  <div>
+    { doc ?
+      <div className="EditDocument">
+        <h4 className="page-header">Editing "{ doc.title }"</h4>
+        <DocumentEditor doc={ doc }/>
+      </div>
+    :
+      <NotFound/>
+    }
   </div>
 );
 
