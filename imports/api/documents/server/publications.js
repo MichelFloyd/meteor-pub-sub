f01edb195db4562d6c8ada4da2846d9e172acb7a
MichelFloyd <michel.floyd@cloaklabs.com>
2017-02-20 13:00:18 -0800

    Removed redundant publication

--- imports/api/documents/server/publications.js
+++ imports/api/documents/server/publications.js
@@ -9,14 +9,9 @@ Meteor.publish('documents.list', () => Documents.find({},{fields: {title: 1}}));
 // while this one includes all keys, but for a single document at a time
 Meteor.publish('documents.view', (_id) => {
   check(_id, String);
-  return [ Documents.find(_id), RelatedDocuments.find({documentId: _id}) ];
-});
-
-Meteor.publish('documents.view.withRelated', (_id) => {
-  check(_id, String);
   // in this simple case we can just return an array of cursors without having to resort to publishComposite.
   // The first is the one document and the second is the cursor of RelatedDocuments
-  return [ Documents.find(_id), RelatedDocuments.find({documentId: _id})];
+  return [ Documents.find(_id), RelatedDocuments.find({documentId: _id}) ];
 });
 
 Meteor.publishComposite('documents.withRelated', (n) => {
